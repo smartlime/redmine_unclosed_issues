78094d18b39784fd886f82fd3b8277defa796ec0
wargene <dave@gmail.ru>
2013-10-25 12:39:20 +0400

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_unclosed_issues do
+  name 'Redmine Unclosed Issues plugin'
+  author 'Evgeny Varnakov'
+  description 'Redmine My Page plugin adds block with issues to close'
+  version '0.0.1'
+  url 'http://deep.msk.ru'
+  author_url 'http://deep.msk.ru'
+end
